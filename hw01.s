e12e7bff16c22cd97ccb6d95d58d14d0ebc9bbbf
kutta97 <yhjin04170@gmail.com>
2021-07-01 18:13:23 +0900

    fmt3, fmt4, fmt5

--- hw01.s
+++ hw01.s
@@ -1,4 +1,4 @@
-fmt1:   .asciz	"++x * z--			: %d\n"
+fmt1:   .asciz	"++x * z--		: %d\n"
 fmt2:   .asciz	"x || --y && z		: %d\n"
 fmt3:   .asciz	"z += x >> 2 + ++y	: %d\n"
 fmt4:   .asciz	"x = y = z = 5		: %d\n"
@@ -11,16 +11,51 @@ main:   save    %sp, -96, %sp
         mov     2, %l1			! y = 2
 		mov     3, %l2			! z = 3
 
-        add     %l0, 1, %o0
+        ! fmt1
+		inc     %l0
+		mov		%l0, %o0
 		mov		%l2, %o1
+		dec		%l2
 		call	.mul
 		nop
 
-		sub		%l2, 1, %l3
 		mov		%o0, %o1
 		set     fmt1, %o0
         call    printf
         nop
+
+		! fmt2
+		dec		%l1
+
+		! fmt3
+		inc		%l1
+		add		%l1, 2, %l3
+		srl		%l0, %l3, %l0
+		add		%l2, %l0, %l2
+		mov		%l2, %o1
+		set     fmt3, %o0
+        call    printf
+
+		! fmt4
+		mov		5, %l2
+		mov		%l2, %l1
+		mov		%l1, %l0
+		mov		%l0, %o1
+		set     fmt4, %o0
+        call    printf
+
+		! fmt5
+		mov		1, %l3
+		mov		2, %l4
+		add		%l3, %l4, %l5
+		mov		2, %l3
+		mov		1, %l4
+		sub		%l3, %l4, %l0
+		mov		%l0, %o1
+		set     fmt5, %o0
+        call    printf
+
+true:	mov		1, %l3
 
         ret
         restore
